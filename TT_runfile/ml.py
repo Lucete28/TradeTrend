8635a3cfc758f801a219a07964aa56c304026920
Lucete28 <2580jhy@naver.com>
2023-06-20 15:49:14 +0900

    ml 후 저장되는 파일 수정

--- TT_runfile/ml.py
+++ TT_runfile/ml.py
@@ -7,7 +7,7 @@ from lightgbm import LGBMRegressor
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import GradientBoostingRegressor
-import json
+import numpy as np
 import xgboost as xgb
 
 df = pd.read_csv('/home/jhy/code/TradeTrend/data/005930_temp.csv')
@@ -32,30 +32,33 @@ xgboost_model = MultiOutputRegressor(xgb.XGBRegressor())
 Accuracy_list = []
 result_list = []
 models = [lin_model, lgbm_model, random_forest_model, knn_model, decision_tree_model, gradient_boosting_model,xgboost_model]
-
+model_names = []
+target  = ['Open', 'High', 'Low', 'Close', 'Volume', 'Change']
 for model in models:
     tmp_list = []
     check= []
-    for i in range(51):
+    for i in range(11):
         if i !=0:
             new_df = df.iloc[:-i]
-            y_train = new_df.loc[1:,['Open','High','Low','Close','Volume','Change']]
+            y_train = new_df.loc[1:,target]
             X_train = new_df.iloc[:-1]
             x_test = new_df.iloc[-1:]
-            y_test = df.iloc[-i][['Open', 'High', 'Low', 'Close', 'Volume', 'Change']]
+            y_test = df.iloc[-i][target]
             model.fit(X_train, y_train)
             predictions = model.predict(x_test)
             check.append((get_sign(predictions[0][-1]),get_sign(y_test[-1])))
         else:
             new_df=df
-            y_train = new_df.loc[1:,['Open','High','Low','Close','Volume','Change']]
+            y_train = new_df.loc[1:,target]
             X_train = new_df.iloc[:-1]
             x_test = new_df.iloc[-1:]
-            y_test = ['nan','nan','nan','nan','nan','nan']
+            
             model.fit(X_train, y_train)
             predictions = model.predict(x_test)
-            tmp_list.append(predictions)
-    result_list.append(tmp_list)
+            tmp_list.append(predictions[0].tolist())
+
+    result_list.append(tmp_list[0])
+
     rise_real_count = 0
     rise_pred_count = 0
     fall_real_count = 0
@@ -76,10 +79,11 @@ for model in models:
         'Rise Accuracy': (rise_real_count / rise_pred_count) * 100 if rise_pred_count != 0 else 'N/A (pred_count가 0입니다)',
         'Fall Accuracy': (fall_real_count / fall_pred_count) * 100 if fall_pred_count != 0 else 'N/A (pred_count가 0입니다)',
     }
-
+    model_names.append(type(model.estimator).__name__)
     Accuracy_list.append(result_dict)
-
+    print(type(model.estimator).__name__)
 df_result = pd.DataFrame(Accuracy_list)
 file_path = '/home/jhy/code/TradeTrend/data/Accuracy.csv'
 df_result.to_csv(file_path, index=False)
-pd.DataFrame(result_list).to_csv('/home/jhy/code/TradeTrend/data/result.csv',index=False)
+print(model_names)
+pd.DataFrame(result_list,index=model_names,columns=target).to_csv('/home/jhy/code/TradeTrend/data/result.csv')
