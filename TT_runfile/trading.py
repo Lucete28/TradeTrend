731fb816b3391a5bfbf37fa667323849a6e5920e
Lucete28 <2580jhy@naver.com>
2023-06-23 14:29:44 +0900

    .

--- TT_runfile/trading.py
+++ TT_runfile/trading.py
@@ -1,18 +1,54 @@
 from airflow.models.variable import Variable
 import pandas as pd 
+import mojito
+import pprint
 
-result_df = pd.read_csv('/home/jhy/code/TradeTrend/data/result.csv')
-# acc7_df = pd.read_csv('/home/jhy/code/TradeTrend/data/Accuracy_7.csv')
-# acc30_df = pd.read_csv('/home/jhy/code/TradeTrend/data/Accuracy_30.csv')
+key = Variable.get("TT_api_key")
+secret = Variable.get("TT_api_secret_key")
+acc_no = Variable.get("TT_acc_no")
 
-result_negative_count = len(result_df[result_df.iloc[:,-1] < 0])
-result_positive_count = len(result_df[result_df.iloc[:,-1] > 0])
+Target_list  =  Variable.get("Target_list")
+values = [tuple(item.strip("()").split(",")) for item in Target_list.split("),")]
+values = [(x[0].strip(), x[1].strip()) for x in values]
 
-if result_positive_count>result_negative_count:
-    '구매 or 존버'
-elif result_negative_count > result_positive_count:
-    '판매 or 안구매'
+broker = mojito.KoreaInvestment(
+    api_key = key,
+    api_secret = secret,
+    acc_no = acc_no,
+    mock = True #만약 모의투자면 이 항목 True로 설정
+)
 
+receipt = []
+resp = broker.fetch_balance()
+asst_icdc_amt = resp['output2'][0]['asst_icdc_amt'] #장내 현금 보유액
+
+
+
+for val in values: 
+    result_df = pd.read_csv(f'/home/jhy/code/TradeTrend/data/{val[0]}_result.csv')
+    # acc7_df = pd.read_csv('/home/jhy/code/TradeTrend/data/Accuracy_7.csv')
+    # acc30_df = pd.read_csv('/home/jhy/code/TradeTrend/data/Accuracy_30.csv')
+
+    result_negative_count = len(result_df[result_df.iloc[:,-1] < 0])
+    result_positive_count = len(result_df[result_df.iloc[:,-1] > 0])
+
+    if result_positive_count>result_negative_count:     #'구매 or 존버'
+        if True: #구매
+            resp = broker.create_market_buy_order(
+            symbol= val[0],
+            quantity=10
+            )
+            receipt.append(resp)
+            
+    elif result_negative_count > result_positive_count: #'판매 or 안구매'
+        if True: #판매
+            resp = broker.fetch_balance()
+            hldg_qty = resp['output1'][0]['hldg_qty']
+            resp = broker.create_market_sell_order(
+            symbol= val[0],
+            quantity=hldg_qty
+            )
+            receipt.append(resp)
 
 
 
