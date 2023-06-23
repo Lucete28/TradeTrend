2d432bbac71493897582837f1b4bc024d3d367a2
Lucete28 <2580jhy@naver.com>
2023-06-23 10:04:15 +0900

    이전작업 완료

--- TT_runfile/ml.py
+++ TT_runfile/ml.py
@@ -9,139 +9,144 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 import numpy as np
 import xgboost as xgb
+from airflow.models.variable import Variable
+Target_list  =  Variable.get("Target_list")
+values = [tuple(item.strip("()").split(",")) for item in Target_list.split("),")]
+values = [(x[0].strip(), x[1].strip()) for x in values]
 
-lin_model = MultiOutputRegressor(LinearRegression())
-lgbm_model = MultiOutputRegressor(LGBMRegressor())
-random_forest_model = MultiOutputRegressor(RandomForestRegressor())
-knn_model = MultiOutputRegressor(KNeighborsRegressor())
-decision_tree_model = MultiOutputRegressor(DecisionTreeRegressor())
-gradient_boosting_model = MultiOutputRegressor(GradientBoostingRegressor())
-xgboost_model = MultiOutputRegressor(xgb.XGBRegressor())
+for val in values:
+    lin_model = MultiOutputRegressor(LinearRegression())
+    lgbm_model = MultiOutputRegressor(LGBMRegressor())
+    random_forest_model = MultiOutputRegressor(RandomForestRegressor())
+    knn_model = MultiOutputRegressor(KNeighborsRegressor())
+    decision_tree_model = MultiOutputRegressor(DecisionTreeRegressor())
+    gradient_boosting_model = MultiOutputRegressor(GradientBoostingRegressor())
+    xgboost_model = MultiOutputRegressor(xgb.XGBRegressor())
 
-def get_sign(a):
-    if a == 'nan':
-        return 2
-    elif a < 0:
-        return -1
-    elif a == 0:
-        return 0
-    else:
-        return 1
-    
-df = pd.read_csv('/home/jhy/code/TradeTrend/data/005930_temp.csv')
-df = df.drop(['Date','Volume_USD/KRW'],axis=1)
-
-Accuracy_list = []
-Accuracy_7 = []
-Accuracy_30 = []
-result_list = []
-models = [lin_model, lgbm_model, random_forest_model, knn_model, decision_tree_model, gradient_boosting_model,xgboost_model]
-model_names = []
-target  = ['Open', 'High', 'Low', 'Close', 'Volume', 'Change']
-for model in models:
-    model_names.append(type(model.estimator).__name__)
-    print(type(model.estimator).__name__)
-    tmp_list = []
-    check= []
-    for i in range(31):
-        if i !=0:
-            new_df = df.iloc[:-i]
-            y_train = new_df.loc[1:,target]
-            X_train = new_df.iloc[:-1]
-            x_test = new_df.iloc[-1:]
-            y_test = df.iloc[-i][target]
-            model.fit(X_train, y_train)
-            predictions = model.predict(x_test)
-            check.append((get_sign(predictions[0][-1]),get_sign(y_test[-1])))
+    def get_sign(a):
+        if a == 'nan':
+            return 2
+        elif a < 0:
+            return -1
+        elif a == 0:
+            return 0
         else:
-            new_df=df
-            y_train = new_df.loc[1:,target]
-            X_train = new_df.iloc[:-1]
-            x_test = new_df.iloc[-1:]
-            
-            model.fit(X_train, y_train)
-            predictions = model.predict(x_test)
-            tmp_list.append(predictions[0].tolist())
+            return 1
     
-    result_list.append(tmp_list[0])
-    ########################################################################################
-# tmp_count_list = [(check[:7]),(check)]
-# for j,k in enumerate(tmp_count_list):
-    
-    rise_real_count_7 = 0
-    rise_pred_count_7 = 0
-    fall_real_count_7 = 0
-    fall_pred_count_7 = 0
-    real_rise_count_7 = 0
-    real_fall_count_7 = 0
-    
-    rise_real_count_30 = 0
-    rise_pred_count_30 = 0
-    fall_real_count_30 = 0
-    fall_pred_count_30 = 0
-    real_rise_count_30 = 0
-    real_fall_count_30 = 0
-    
-    for j, i in enumerate(check):
-        if j<=7:
-            if i[0] == 1:
-                rise_pred_count_7 += 1
-                rise_pred_count_30 += 1
+    df = pd.read_csv(f'/home/jhy/code/TradeTrend/data/{val[0]}_temp.csv')
+    df = df.drop(['Date','Volume_USD/KRW'],axis=1)
+
+    Accuracy_list = []
+    Accuracy_7 = []
+    Accuracy_30 = []
+    result_list = []
+    models = [lin_model, lgbm_model, random_forest_model, knn_model, decision_tree_model, gradient_boosting_model,xgboost_model]
+    model_names = []
+    target  = ['Open', 'High', 'Low', 'Close', 'Volume', 'Change']
+    for model in models:
+        model_names.append(type(model.estimator).__name__)
+        print(type(model.estimator).__name__)
+        tmp_list = []
+        check= []
+        for i in range(31):
+            if i !=0:
+                new_df = df.iloc[:-i]
+                y_train = new_df.loc[1:,target]
+                X_train = new_df.iloc[:-1]
+                x_test = new_df.iloc[-1:]
+                y_test = df.iloc[-i][target]
+                model.fit(X_train, y_train)
+                predictions = model.predict(x_test)
+                check.append((get_sign(predictions[0][-1]),get_sign(y_test[-1])))
+            else:
+                new_df=df
+                y_train = new_df.loc[1:,target]
+                X_train = new_df.iloc[:-1]
+                x_test = new_df.iloc[-1:]
+                
+                model.fit(X_train, y_train)
+                predictions = model.predict(x_test)
+                tmp_list.append(predictions[0].tolist())
+        
+        result_list.append(tmp_list[0])
+        ########################################################################################
+    # tmp_count_list = [(check[:7]),(check)]
+    # for j,k in enumerate(tmp_count_list):
+        
+        rise_real_count_7 = 0
+        rise_pred_count_7 = 0
+        fall_real_count_7 = 0
+        fall_pred_count_7 = 0
+        real_rise_count_7 = 0
+        real_fall_count_7 = 0
+        
+        rise_real_count_30 = 0
+        rise_pred_count_30 = 0
+        fall_real_count_30 = 0
+        fall_pred_count_30 = 0
+        real_rise_count_30 = 0
+        real_fall_count_30 = 0
+        
+        for j, i in enumerate(check):
+            if j<=7:
+                if i[0] == 1:
+                    rise_pred_count_7 += 1
+                    rise_pred_count_30 += 1
+                    if i[1] == 1:
+                        rise_real_count_7 += 1
+                        rise_real_count_30 += 1
+                elif i[0] == -1:
+                    fall_pred_count_7 += 1
+                    fall_pred_count_30 += 1
+                    if i[1] == -1:
+                        fall_real_count_7 += 1
+                        fall_real_count_30 += 1
                 if i[1] == 1:
-                    rise_real_count_7 += 1
-                    rise_real_count_30 += 1
-            elif i[0] == -1:
-                fall_pred_count_7 += 1
-                fall_pred_count_30 += 1
-                if i[1] == -1:
-                    fall_real_count_7 += 1
-                    fall_real_count_30 += 1
-            if i[1] == 1:
-                real_rise_count_7+=1
-                real_rise_count_30+=1
-            elif i[1] == -1:
-                real_fall_count_7+=1
-                real_fall_count_30+=1
-        else: 
-            if i[0] == 1:
-                rise_pred_count_30 += 1
+                    real_rise_count_7+=1
+                    real_rise_count_30+=1
+                elif i[1] == -1:
+                    real_fall_count_7+=1
+                    real_fall_count_30+=1
+            else: 
+                if i[0] == 1:
+                    rise_pred_count_30 += 1
+                    if i[1] == 1:
+                        rise_real_count_30 += 1
+                elif i[0] == -1:
+                    fall_pred_count_30 += 1
+                    if i[1] == -1:
+                        fall_real_count_30 += 1
                 if i[1] == 1:
-                    rise_real_count_30 += 1
-            elif i[0] == -1:
-                fall_pred_count_30 += 1
-                if i[1] == -1:
-                    fall_real_count_30 += 1
-            if i[1] == 1:
-                real_rise_count_30+=1
-            elif i[1] == -1:
-                real_fall_count_30+=1
-            
+                    real_rise_count_30+=1
+                elif i[1] == -1:
+                    real_fall_count_30+=1
+                
 
 
-    result_dict_7 = {
-        'Model Name': type(model.estimator).__name__,
-        'real_rise' : real_rise_count_7,
-        'rise_pred_count' : rise_pred_count_7,
-        'real_fall' : real_fall_count_7,
-        'fall_pred_count' : fall_pred_count_7,
-        'Rise_pred Accuracy': (rise_real_count_7 / rise_pred_count_7) * 100 if rise_pred_count_7 != 0 else 'N/A (pred_count가 0입니다)',
-        'Fall_pred Accuracy': (fall_real_count_7 / fall_pred_count_7) * 100 if fall_pred_count_7 != 0 else 'N/A (pred_count가 0입니다)',
-    }
-    Accuracy_7.append(result_dict_7)
-    df_acc_7 = pd.DataFrame(Accuracy_7)
-    df_acc_7.to_csv('/home/jhy/code/TradeTrend/data/Accuracy_7.csv',index=False)
-    
-    result_dict_30 = {
-        'Model Name': type(model.estimator).__name__,
-        'real_rise' : real_rise_count_30,
-        'rise_pred_count' : rise_pred_count_30,
-        'real_fall' : real_fall_count_30,
-        'fall_pred_count' : fall_pred_count_30,
-        'Rise_pred Accuracy': (rise_real_count_30 / rise_pred_count_30) * 100 if rise_pred_count_30 != 0 else 'N/A (pred_count가 0입니다)',
-        'Fall_pred Accuracy': (fall_real_count_30 / fall_pred_count_30) * 100 if fall_pred_count_30 != 0 else 'N/A (pred_count가 0입니다)',
-    }
-    Accuracy_30.append(result_dict_30)
-    df_acc_30 = pd.DataFrame(Accuracy_30)
-    df_acc_30.to_csv('/home/jhy/code/TradeTrend/data/Accuracy_30.csv',index=False)
+        result_dict_7 = {
+            'Model Name': type(model.estimator).__name__,
+            'real_rise' : real_rise_count_7,
+            'rise_pred_count' : rise_pred_count_7,
+            'real_fall' : real_fall_count_7,
+            'fall_pred_count' : fall_pred_count_7,
+            'Rise_pred Accuracy': (rise_real_count_7 / rise_pred_count_7) * 100 if rise_pred_count_7 != 0 else 'N/A (pred_count가 0입니다)',
+            'Fall_pred Accuracy': (fall_real_count_7 / fall_pred_count_7) * 100 if fall_pred_count_7 != 0 else 'N/A (pred_count가 0입니다)',
+        }
+        Accuracy_7.append(result_dict_7)
+        df_acc_7 = pd.DataFrame(Accuracy_7)
+        df_acc_7.to_csv(f'/home/jhy/code/TradeTrend/data/{val[0]}_Accuracy_7.csv',index=False)
+        
+        result_dict_30 = {
+            'Model Name': type(model.estimator).__name__,
+            'real_rise' : real_rise_count_30,
+            'rise_pred_count' : rise_pred_count_30,
+            'real_fall' : real_fall_count_30,
+            'fall_pred_count' : fall_pred_count_30,
+            'Rise_pred Accuracy': (rise_real_count_30 / rise_pred_count_30) * 100 if rise_pred_count_30 != 0 else 'N/A (pred_count가 0입니다)',
+            'Fall_pred Accuracy': (fall_real_count_30 / fall_pred_count_30) * 100 if fall_pred_count_30 != 0 else 'N/A (pred_count가 0입니다)',
+        }
+        Accuracy_30.append(result_dict_30)
+        df_acc_30 = pd.DataFrame(Accuracy_30)
+        df_acc_30.to_csv(f'/home/jhy/code/TradeTrend/data/{val[0]}_Accuracy_30.csv',index=False)
 
-pd.DataFrame(result_list,index=model_names,columns=target).to_csv('/home/jhy/code/TradeTrend/data/result.csv')
+    pd.DataFrame(result_list,index=model_names,columns=target).to_csv(f'/home/jhy/code/TradeTrend/data/{val[0]}_result.csv')
--- TT_runfile/update_naver_raw.py
+++ TT_runfile/update_naver_raw.py
@@ -2,29 +2,34 @@ from airflow.models.variable import Variable
 import openai
 import pandas as pd
 openai.api_key  =  Variable.get("gpt_api_key")
+Target_list  =  Variable.get("Target_list")
+values = [tuple(item.strip("()").split(",")) for item in Target_list.split("),")]
+values = [(x[0].strip(), x[1].strip()) for x in values]
 
-gpt_ans = []
+for val in values:
+    
+    gpt_ans = []
 
-temp_df = pd.read_csv('/home/jhy/code/TradeTrend/data/temp4.csv')
-raw_df = pd.read_csv('/home/jhy/code/TradeTrend/data/news_raw2.csv')
+    temp_df = pd.read_csv(f'/home/jhy/code/TradeTrend/data/{val[0]}_temp4.csv')
+    raw_df = pd.read_csv(f'/home/jhy/code/TradeTrend/data/{val[0]}_news_raw2.csv')
 
-ans_list = raw_df.iloc[:, 1]
-for i, ans in enumerate(ans_list):
-    if len(str(ans)) > 5 or (float(ans)>1 or float(ans)<0):
-        messages = []
-        a = temp_df.iloc[i,1]
-        content = f'{a} 삼성전자 관련 뉴스기사 제목인데 삼성전자 주식에 미칠 긍정도의 평균을 0에서 1사이 소숫점 두자리까지 나타내 float값만'
-        messages.append({"role": "user", "content": content})
+    ans_list = raw_df.iloc[:, 1]
+    for i, ans in enumerate(ans_list):
+        if len(str(ans)) > 5 or (float(ans)>1 or float(ans)<0):
+            messages = []
+            a = temp_df.iloc[i,1]
+            content = f'{a} {val[1]} 관련 뉴스기사 제목인데 {val[1]} 주식에 미칠 긍정도의 평균을 0에서 1사이 소숫점 두자리까지 나타내 float값만'
+            messages.append({"role": "user", "content": content})
 
-        completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=messages
-        )
+            completion = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=messages
+            )
 
-        chat_response = completion.choices[0].message.content
-        gpt_ans.append(chat_response)
-        messages.append({"role": "assistant", "content": chat_response})
+            chat_response = completion.choices[0].message.content
+            gpt_ans.append(chat_response)
+            messages.append({"role": "assistant", "content": chat_response})
 
-        # 기존 값을 새로운 값으로 교체
-        raw_df.iloc[i,1] = chat_response
-        raw_df.to_csv('/home/jhy/code/TradeTrend/data/news_raw2.csv', index=False)
+            # 기존 값을 새로운 값으로 교체
+            raw_df.iloc[i,1] = chat_response
+            raw_df.to_csv(f'/home/jhy/code/TradeTrend/data/{val[0]}_news_raw2.csv', index=False)
